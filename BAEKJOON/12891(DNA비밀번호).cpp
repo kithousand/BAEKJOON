7e1a0c9a37019239b4ce8b5da5129f9ff3193051
ki_thousand <kicheon632@naver.com>
2023-09-13 18:01:55 +0900

    add file 12891(DNA비밀번호).cpp timeout

---
+++ BAEKJOON/12891(DNA비밀번호).cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <stack>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+//https://www.acmicpc.net/problem/1874
+
+
+int main()
+{
+	int S, P, A, C, G, T;
+
+	int a, c, g, t;
+	int result = 0;
+
+	cin >> S >> P;
+
+	int num = S - P + 1;
+
+	vector<char> DNA(S);
+
+	for (int i = 0; i < S; i++)
+	{
+		cin >> DNA[i];
+	}
+
+	cin >> A >> C >> G >> T;
+	a = A;
+	c = C;
+	g = G;
+	t = T;
+
+	for (int i = 0; i < num; i++)
+	{
+		//작은배열 돌면서
+		for (int j = 0; j < P ; j++)
+		{
+			//작은배열 안에서 A,C,G,T,만족하는지 확인
+			switch (DNA[i+j])
+			{
+			case 'A':
+				A--;
+				break;
+			case 'C':
+				C--;
+				break;
+			case 'G':
+				G--;
+				break;
+			case 'T':
+				T--;
+				break;
+			}
+		}
+		if (A <= 0 && C <= 0 && G <= 0 && T <= 0)
+		{
+			result++;
+		}
+		//다시 입력받값으로 초기화하는법
+		A = a;
+		C = c;
+		G = g;
+		T = t;
+	}
+	std::cout << result << "\n";
+}
