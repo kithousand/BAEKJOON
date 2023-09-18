53756843c83e2678e2a2def303cd1b53ed70312c
kithousand <kicheon632@naver.com>
2023-09-18 22:31:56 +0900

    12891(DNA비밀번호).cpp 수정

--- BAEKJOON/12891(DNA비밀번호).cpp
+++ BAEKJOON/12891(DNA비밀번호).cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-#include <stack>
 #include <algorithm>
 #include <vector>
 #include <string>
-#include <time.h>
 using namespace std;
 
 //https://www.acmicpc.net/problem/1874
@@ -11,9 +9,53 @@ using namespace std;
 
 int main()
 {
-	int TIME = 0;
 
+	
+		int S, P, A, C, G, T;
 
+		cin >> S >> P;
+		vector<char> DNA(S);
+		vector<int> countA(S + 1, 0);
+		vector<int> countC(S + 1, 0);
+		vector<int> countG(S + 1, 0);
+		vector<int> countT(S + 1, 0);
+
+		for (int i = 0; i < S; i++) {
+			cin >> DNA[i];
+			countA[i + 1] = countA[i] + (DNA[i] == 'A');
+			countC[i + 1] = countC[i] + (DNA[i] == 'C');
+			countG[i + 1] = countG[i] + (DNA[i] == 'G');
+			countT[i + 1] = countT[i] + (DNA[i] == 'T');
+		}
+
+		cin >> A >> C >> G >> T;
+
+		int result = 0;
+
+		for (int i = 0; i < S - P + 1; i++) {
+			int subA = countA[i + P] - countA[i];
+			int subC = countC[i + P] - countC[i];
+			int subG = countG[i + P] - countG[i];
+			int subT = countT[i + P] - countT[i];
+
+			if (subA >= A && subC >= C && subG >= G && subT >= T) {
+				result++;
+			}
+		}
+
+		cout << result << "\n";
+
+		return 0;
+	
+
+	//처음내가짠코드
+	//작은 모든 배열을 계속 확인하기때문에 
+	//시간복잡도가 늘어난다
+	//슬라이딩윈도우기법을 활용하여
+	//전체 DNA 서열을 한 번만 스캔하면서 A, C, G, T의 누적 개수를 구합니다.이를 누적 배열로 저장합니다.
+	//그런 다음, 부분 서열을 슬라이딩 윈도우 기법을 사용하여 이동하면서 해당 부분 서열 내의 A, C, G, T 개수를 빠르게 계산합니다.
+	//각 부분 서열에 대해 A, C, G, T의 개수를 빠르게 계산할 수 있으므로, 주어진 A, C, G, T 값과 비교하여 만족하는 부분 서열을 찾습니다.
+	/*
 	int S, P, A, C, G, T;
 
 	int a, c, g, t;
@@ -69,8 +111,5 @@ int main()
 		T = t;
 	}
 	std::cout << result << "\n";
-
-
-
-
+	*/
 }
